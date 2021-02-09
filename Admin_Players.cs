91eeee2a5a1354120ecc42785639aecdec7f1643
jazzy348 <dj_ej_9@hotmail.co.uk>
2021-02-09 23:51:33 +0000

    Add files via upload

---
+++ Admin_Players.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Admin_Players : UdonSharpBehaviour
+{
+    [SerializeField] GameObject[] adminObjects;
+    [SerializeField] string[] adminUsers;
+
+
+    void Start()
+    {
+        foreach(string adminPlayers in adminUsers) {
+            if (Networking.LocalPlayer.displayName == adminPlayers) {
+                 foreach(GameObject go in adminObjects) {
+                  go.SetActive(true);
+                 }
+             }
+         }
+    }
+}
